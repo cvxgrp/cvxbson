b7dbe150f7ccf5fc07c89ca40b04be45d2e58000
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-25 07:49:53 -0700

    syncing json and bson

--- cvx/bson/file.py
+++ cvx/bson/file.py
@@ -1,52 +1,86 @@
 # -*- coding: utf-8 -*-
 """
-Dataclass to simplify dealing with bson files.
+Tools to support working with bson files
 """
 from __future__ import annotations
 
-from dataclasses import dataclass
+import io
 from pathlib import Path
-from typing import Dict
 
-from .io import read_bson
+import numpy as np
+import simple_bson as bson
+import pyarrow as pa
+import pyarrow.parquet as pq
 
-
-def build_bson(file):
+def read_bson(file: Path):
     """
-    Construct the frozen Dataclass for a BsonFile with this builder.
+    Read a bson file and prepare the bson_document needed
 
     Args:
-        file: Path to *.bson file.
+        file:
 
     Returns:
-        _BsonFile object.
+        a bson-document
     """
-    return _BsonFile(file, dict(read_bson(file)))
+    assert file.exists()
+
+    with open(file, mode="rb") as openfile:
+        # Reading from bson file
+        bson_document = openfile.read()
+        data = bson.loads(bson_document)
+
+        for name, value in data.items():
+            xxx = np.array([row for row in pq.read_table(io.BytesIO(value))])
+            yield name, xxx
 
 
-@dataclass(frozen=True)
-class _BsonFile:
+
+def write_bson(file: Path, dic, compression="zstd"):
     """
-    A BsonFile contains the keys meta, tables and timeseries.
+    Write dictionary into bson file
 
-    Attributes:
-        file: Path to *.bson file.
-        data: Dict with data stored in a file.
+    Args:
+        file: file
+        dictionary of polars dataframes
+        logger: logger
+
     """
 
-    file: Path
-    data: Dict = None
+    def to_binary(matrix: np.ndarray):
+        """
+        byte-stream of a frame
 
-    @property
-    def name(self):
-        """
-        Name of the file.
-        """
-        return self.file.stem
+        Args:
+            matrix: the frame converted to ipc
 
-    @property
-    def parent(self):
+        Returns:
+            the bytestream
         """
-        Parent of the file (e.g. path).
-        """
-        return self.file.parent
+
+        with io.BytesIO() as buffer:
+            if isinstance(matrix, np.ndarray):
+
+                arrays = [
+                    pa.array(col)  # Create one arrow array per row
+                    for col in matrix
+                ]
+
+                table = pa.Table.from_arrays(
+                    arrays,
+                    names=[str(i) for i in range(np.shape(arrays)[0])]
+                    # give name to each row
+                )
+
+                pq.write_table(table, buffer, compression=compression)
+
+            else:
+                raise ValueError("Bson only supports numpy arrays")
+
+            return buffer.getvalue()
+
+    content = bson.dumps({name: to_binary(value) for name, value in dic.items()})
+
+    with open(file=file, mode="wb") as bson_file:
+        bson_file.write(content)
+
+    return file
--- tests/test_bson.py
+++ tests/test_bson.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 import numpy as np
 import pytest
 
-from cvx.bson.io import read_bson
-from cvx.bson.io import write_bson
+from cvx.bson.file import read_bson
+from cvx.bson.file import write_bson
 
 
 @pytest.fixture()
@@ -23,45 +23,3 @@ def test_write(random_matrix, tmp_path):
 
     assert set(x.keys()) == {"B"}
     np.allclose(x["B"], random_matrix)
-
-    #write_bson("maffay.bson", dic)
-
-
-# def test():
-#     import numpy as np
-#     import pyarrow as pa
-#     import pyarrow.parquet as pq
-#
-#     matrix = np.random.rand(5, 3)
-#     arrays = [
-#         pa.array(row)  # Create one arrow array per row
-#         for row in matrix
-#     ]
-#     print(arrays)
-#     #assert False
-#
-#     table = pa.Table.from_arrays(
-#         arrays,
-#         names=[str(i) for i in range(len(arrays))]  # give names to each columns
-#     )
-#     print(table)
-#     #table = pa.array(matrix)
-#
-#
-#     #assert False
-#
-#     # Save it:
-#     pq.write_table(table, 'table.pq')
-#
-#     # Read it back as numpy:
-#     table_from_parquet = pq.read_table('table.pq')
-#     print(table_from_parquet)
-#
-#
-#     matrix_from_parquet = table_from_parquet.to_pandas().T.values
-#     print(matrix_from_parquet)
-#
-#     #assert False
-#     #    .T.to_numpy()
-#
-#     assert np.allclose(matrix, matrix_from_parquet)
