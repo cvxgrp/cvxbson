f42d384e8f4ca6f07d332a15d399c5d0fd838acd
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-07-30 11:42:28 -0700

    Update smallest_eigenvalue.py

--- cli/smallest_eigenvalue.py
+++ cli/smallest_eigenvalue.py
@@ -7,15 +7,8 @@ from cvx.bson import read_bson
 def smallest_ev(bson_file) -> None:
     """Print FILENAME if the file exists."""
     data = read_bson(bson_file)
-
-    #    json_data = json.load(f)
-
-    # a_restored = np.asarray(json_data["a"])
-
-    w, v = np.linalg.eigh(data["cov"])
-    idx = w.argsort()
-    w = w[idx]
-    return w[0]
+    w, _ = np.linalg.eigh(data["cov"])
+    return np.min(w)
 
 
 def main():
