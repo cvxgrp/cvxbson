906b1915eb144612629de24e3de98fd7626a5cff
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-09-27 22:15:34 -0700

    simplifying the book construction

--- book/sphinx/conf.py
+++ book/sphinx/conf.py
@@ -16,6 +16,7 @@ extensions = [
     "autoapi.extension",
 ]
 
+
 source_dir = "cvx"
 autoapi_type = "python"
 autoapi_dirs = [f"../../{source_dir}"]
@@ -33,8 +34,8 @@ napoleon_google_docstring = True
 napoleon_numpy_docstring = True
 
 # HTML -----------------------------------------------------------------
-html_favicon = "_static/logo.jpeg"
-html_logo = "_static/logo.jpeg"
+# html_favicon = "_static/logo.jpeg"
+# html_logo = "_static/logo.jpeg"
 html_title = "cvxbson Documentation"
 html_show_sourcelink = True
 html_css_files = [
