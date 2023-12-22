e11d4ed92ea72b8ddc3c307bdf7b44dad04bbc56
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-12-22 12:03:19 +0400

    pandas dataframes supported

---
+++ tests/test_pandas.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from cvx.bson.file import from_bson, to_bson
+
+
+@pytest.fixture()
+def data():
+    return {
+        "frame": pd.DataFrame(data=np.random.rand(5, 2)),
+        "numpy": np.random.rand(5, 2),
+        "frame_with_time": pd.DataFrame(
+            data=np.random.rand(2, 2),
+            index=[pd.Timestamp("2020-01-01"), pd.Timestamp("2022-01-01")],
+        ),
+    }
+
+
+def assert_equal(obj1, obj2):
+    assert type(obj1) == type(obj2)
+
+    if isinstance(obj1, pd.DataFrame):
+        pd.testing.assert_frame_equal(obj1, obj2)
+
+    if isinstance(obj1, np.ndarray):
+        np.testing.assert_array_equal(obj1, obj2)
+
+
+def test_roundtrip(data):
+    """
+    Testing the roundtrip
+
+    Args:
+        data: Fixture exposing a dictionary of data
+    """
+    reproduced = from_bson(to_bson(data))
+    for key, value in reproduced.items():
+        assert_equal(value, data[key])
+
+
+def test_file(data, tmp_path):
+    with open(file=tmp_path / "xxx.bson", mode="wb") as bson_file:
+        bson_file.write(to_bson(data))
+
+    with open(file=tmp_path / "xxx.bson", mode="rb") as bson_file:
+        reproduced = from_bson(bson_file.read())
+
+    for key, value in reproduced.items():
+        assert_equal(reproduced[key], data[key])
